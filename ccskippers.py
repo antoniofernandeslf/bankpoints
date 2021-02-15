f945f9fce319d92f826a0e516da413ee808501b7
tom <antonio.fernandes.lf@hotmail.com>
2021-02-15 15:34:39 -0300

    Criação e execução do pipeline

--- ccskippers.py
+++ ccskippers.py
@@ -23,3 +23,21 @@ def calculate_points(element):
         defaulter_pts += 1
 
     return key_name, defaulter_pts
+
+
+p = beam.Pipeline()
+
+defaulter = (
+    p
+#Leitura dos dados para a collection
+    | 'Read data' >> beam.io.ReadFromText('datasets/cards.txt', skip_header_lines=1)
+#retorna "lista" de clientes e seus pontos no por mês
+    | 'Calculate points' >> beam.Map(calculate_points)
+#Agrupa todos os clientes com a mesma chave(criada na função calculate) somando os pontos
+    | 'Combine poits' >> beam.CombinePerKey(sum)
+#Filtra apenas os que possuem pontos > 0
+    | 'Filter defaulters' >> beam.Filter(lambda element: element[1] > 0)
+#Salva o resultado
+    | 'Write credit card data' >> beam.io.WriteToText("output/cc_skippers")
+)
+p.run()
